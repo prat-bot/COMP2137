0e35d5b8bed9dd3641666ce8d148f02c7361ac63
Pratik Thapa Magar <200594866@student.georgianc.on.ca>
2025-04-03 13:51:48 -0400

    Lab 3 assignment

--- lab3.sh
+++ lab3.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
-# Server configuration deployment script
-
-# Enable error handling
-set -e
-
-verbose_flag=""
-if [[ "$1" == "-verbose" ]]; then
-    verbose_flag="-verbose"
-    echo "Verbose mode enabled"
-    shift
-fi
-
-# Function to handle errors
-exit_on_error() {
-    echo "Error: $1"
-    exit 1
-}
-
+# This script runs the configure-host.sh script from the current directory to modify 2 servers and update the local /etc/hosts file
 scp configure-host.sh remoteadmin@server1-mgmt:/root
-ssh remoteadmin@server1-mgmt "bash /root/configure-host.sh $verbose_flag -name loghost -ip 192.168.16.10 -hostentry webhost 192.168.16.11"
+ssh remoteadmin@server1-mgmt "bash /root/configure-host.sh $verbose_flag -name loghost -ip 192.168.16.10 -hostentry webhost 192.168.16.4"
 scp configure-host.sh remoteadmin@server2-mgmt:/root
-ssh remoteadmin@server2-mgmt "bash /root/configure-host.sh $verbose_flag -name webhost -ip 192.168.16.11 -hostentry loghost 192.168.16.10"
-sudo ./configure-host.sh $verbose_flag -hostentry loghost 192.168.16.10
-sudo ./configure-host.sh $verbose_flag -hostentry webhost 192.168.16.11
+ssh remoteadmin@server2-mgmt "bash /root/configure-host.sh $verbose_flag -name webhost -ip 192.168.16.11 -hostentry loghost 192.168.16.3"
+sudo ./configure-host.sh $verbose_flag -hostentry loghost 192.168.16.3
+sudo ./configure-host.sh $verbose_flag -hostentry webhost 192.168.16.4
 
 echo "Lab3 Done!!!!!"
